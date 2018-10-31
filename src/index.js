e0fd46d1c71c44dd722085293f0a0951654e2fa9
WW930\A701895 <maciej.ostrowski@atos.net>
2018-10-31 14:01:05 +0100

    Installed nodemon. Changes at package.json. Changes at index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,73 @@
-import { GraphQLServer } from 'graphql-yoga'
+import {
+    GraphQLServer
+} from 'graphql-yoga'
+
+// Scalar Types: String, Int, Float, ID, Boolean
 
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        hello: String!
+        id: ID!
         name: String!
-        bio: String!
+        age: Int!
+        employed: Boolean!
+        average: Float!
         location: String!
+        bio: String!
+        title: String!
+        price: Float!
+        releaseYear: Int!
+        rating: Float!
+        inStock: Boolean!
     }
 `
+
 
 // Resolvers
 const resolvers = {
     Query: {
-        hello() {
-            return 'This is my first query!'
+        id() {
+            return 'abc123'
         },
         name() {
-            return 'Rick Grimes'
+            return 'Andrew'
         },
-        bio() {
-            const Bio = 'I live in Atlanta'
-
-            return Bio;
+        age() {
+            return 27
+        },
+        employed() {
+            return true
+        },
+        average() {
+            return 3.87
         },
         location() {
             return 'Atlanta'
+        },
+        bio() {
+            return 'I live in Atlanta';
+        },
+        title() {
+            return 'My Title'
+        },
+        price() {
+            return 4.98
+        },
+        releaseYear() {
+            return 598
+        },
+        rating() {
+            return 4.02
+        },
+        inStock() {
+            return true
         }
     }
 }
 
-const server = new GraphQLServer ({
+const server = new GraphQLServer({
     typeDefs,
-    resolvers
+    resolvers,
 })
 
 server.start(() => {
